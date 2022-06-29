8daf09581d53c5fbe63b9d221288324beabb2a05
David Barber <lowellbarbers2@gmail.com>
2022-06-29 16:13:28 -0400

    he

--- line.py
+++ line.py
@@ -1,28 +1,32 @@
 from nuro_arm import RobotArm
 robot = RobotArm()
 
+x = 0.19
+y = 0.0381
+z = 0.27
+
 run_val = [0.19,0,0.27]
 grasp_jpos = [0,-0.255,-1.349,-1.466,0.029]
-ee_pos_drop0 = [0.19,0.0381*3, .27]
-ee_pos_drop1 = [0.19,0.0381*2,0.27]
-ee_pos_drop2 = [0.19,0.0381,0.27]
-ee_pos_drop3 = [0.19,0,0.27]
-ee_pos_drop4 = [0.19,-0.0381,0.27]
-ee_pos_drop5 = [0.19,-0.0381*2,0.27]
-ee_pos_drop6 = [0.19,-0.0381*3,0.27]
+ee_pos_drop0 = [x,0.0381*3, z]
+ee_pos_drop1 = [x,0.0381*2, z]
+ee_pos_drop2 = [x,0.0381, z]
+ee_pos_drop3 = [x,0,z]
+ee_pos_drop4 = [x,-0.0381,z]
+ee_pos_drop5 = [x,-0.0381*2,z]
+ee_pos_drop6 = [x,-0.0381*3,z]
 
 for i in range(7):
     if i == 0:
         run_val = ee_pos_drop0
     elif i == 1:
         run_val = ee_pos_drop1
-    elif i==2:
+    elif i == 2:
         run_val == ee_pos_drop2
     elif i == 3:
         run_val == ee_pos_drop3
     elif i == 4:
         run_val == ee_pos_drop4
-    elif i== 5:
+    elif i == 5:
         run_val == ee_pos_drop5
     else:
         run_val == ee_pos_drop6
