9e433c667ea8d0b62307aebb9dceacb80d902d2e
David Barber <lowellbarbers2@gmail.com>
2022-07-13 11:38:19 -0400

    addition of "move" and "Connect_4_grid"

--- Connect_4_Grid.py
+++ Connect_4_Grid.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 
 # create the cropped image based on downloaded image
 img = cv2.imread(r"C:\Users\lowel\Downloads\connect 4.jpg")
-cropped_board = img[110:550, 0:520]
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cropped_board = hsv_img[110:550, 0:520]
+#cropped_board = img[110:350, 0:520]
 shape = cropped_board.shape
 
 # find values that are necessary to access specific pixels
@@ -22,21 +24,38 @@ arr = np.zeros([6, 7])
 
 # upload values to the array
 # **opencv Interprets colors in BGR and Matplotlib is in RGB**
+def average_color(i, j):
+    y = y1 + (height * i)
+    x = x1 + (width * j)
+    n = 1
+    old_value = 0
+
+    for p in range(10):
+        for o in range(10):
+            colors = cropped_board[y - 5 + o, x - 5 + p]
+            new = colors[0]
+            average = (new + old_value) / n
+            n += 1
+            old_value += new
+            cropped_board[y - 5 + o, x - 5 + p] = [0, 0, 100]
+    return average
+
+
+# upload values to the array
+# **opencv Interprets colors in BGR and Matplotlib is in RGB**
 for i in range(rows):
     for j in range(columns):
-        y = y1 + (height * i)
-        x = x1 + (width * j)
-        colors = cropped_board[y, x]
+        h = average_color(i, j)
 
-        if ((colors[2] < 255) & (colors[2] > 230)) & ((colors[1] < 230) & (colors[1] > 200)) & ((colors[0] < 20)):
+        if (h < 30) & (h > 11):
             arr[i][j] = 1
 
-        elif ((colors[2] < 255) & (colors[2] > 230)) & ((colors[1] < 100) & (colors[1] > 40)) & ((colors[0] < 20)):
+        elif (h < 10) & (h > 0):
+
             arr[i][j] = -1
 
         else:
             arr[i][j] = 0
-        cropped_board[y, x] = [255, 255, 255]
 
 
 print(arr)
@@ -52,7 +71,7 @@ def diag1_check(i, j):
     for x in range(3):
         if ((i + m) > 5) or ((j + m) > 6):
             break
-        elif (arr[i + m][j + m] == 1):
+        elif arr[i + m][j + m] == 1:
             evaluation += 1
             m += 1
         else:
@@ -62,7 +81,7 @@ def diag1_check(i, j):
     for x in range(3):
         if ((i - n) < 0) or ((j - n) < 0):
             break
-        elif (arr[i - n][j - n] == 1):
+        elif arr[i - n][j - n] == 1:
             evaluation += 1
             n += 1
         else:
@@ -75,9 +94,9 @@ def diag2_check(i, j):
     # checks values to the right and up of the point
     m = 1
     for x in range(3):
-        if ((i + m) < 0) or ((j + m) > 6):
+        if ((i - m) < 0) or ((j + m) > 6):
             break
-        elif (arr[i - m][j + m] == 1):
+        elif arr[i - m][j + m] == 1:
             evaluation += 1
             m += 1
         else:
@@ -85,9 +104,9 @@ def diag2_check(i, j):
     # checks values to the left and up of the point
     n = 1
     for x in range(3):
-        if ((i - n) > 0) or ((j - n) < 0):
+        if ((i + n)>5 ) or ((j - n) < 0):
             break
-        elif (arr[i + n][j - n] == 1):
+        elif arr[i + n][j - n] == 1:
             evaluation += 1
             n += 1
         else:
@@ -115,9 +134,9 @@ def horizontal_check(i, j):
     # checks values to the right of the point
     m = 1
     for x in range(3):
-        if (arr[i][j + m] == 1):
-            if (j + m) > 5:
-                break
+        if (j + m) > 6:
+            break
+        elif arr[i][j + m] == 1:
             evaluation += 1
             m += 1
         else:
@@ -125,9 +144,9 @@ def horizontal_check(i, j):
     # checks values to the left of the point
     n = 1
     for x in range(3):
-        if (arr[i][j - n] == 1):
-            if (j - n) < 0:
-                break
+        if (j - n) < 0:
+            break
+        elif arr[i][j - n] == 1:
             evaluation += 1
             n += 1
         else:
@@ -139,16 +158,15 @@ evaluations = np.zeros((7), dtype=int)
 for x in range(7):
     for y in reversed(range(6)):
         if (arr[y][x] == 0):
-            Diagonal1 = diag1_check(y, x)
-            #Diagonal2 = diag2_check(y, x)
-            #horizontal = horizontal_check(y, x)
-            #verticle = verticle_check(y, x)
-            #final_eval = max(Diagonal1, Diagonal2, horizontal, verticle)
-            #evaluations[j] = final_eval
-            print(Diagonal1)
+            diagonal1 = diag1_check(y, x)
+            diagonal2 = diag2_check(y, x)
+            horizontal = horizontal_check(y, x)
+            verticle = verticle_check(y, x)
+            final_eval = max(diagonal1, diagonal2, horizontal, verticle)
+            evaluations[x] = final_eval
             break
 
-
+print(evaluations)
 
 
 cv2.imshow('cropped', cropped_board)
