9c1828b0e3f06a56c755bac87acf34222ebd7cfa
David Barber <lowellbarbers2@gmail.com>
2022-07-26 13:21:56 -0400

    Calibrate Camera by running: Camera.py, draw_corners.py, camera_caliration.p

--- Camera Calibration/Connect_4_Grid.py
+++ Camera Calibration/Connect_4_Grid.py
@@ -6,11 +6,8 @@ from nuro_arm import RobotArm
 import pickle
 import random
 
-robot = RobotArm()
-cam_jpos = [0.00, -1.59174028,  1.03044239, 2.0943951, -0.09215338]
 
-
-def simple_ai():
+def take_pic():  # takes a picture and processes it into an array, returns the array
     robot.move_arm_jpos(cam_jpos)
 
     cap = cv2.VideoCapture(1)
@@ -23,7 +20,6 @@ def simple_ai():
         # Display the resulting frame
         # cv2.imshow('preview',frame)
         if i == 4:
-
             # Waits for a user input to quit the application
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -36,17 +32,14 @@ def simple_ai():
     src = cv2.imread(r"frame.jpg")
     img = cv2.rotate(src, cv2.ROTATE_180)
     hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    # cropped_board = hsv_img[110:550, 0:520]
-    # cropped_board = hsv_img[110:385, 160:513]
 
+    # undistort the image using values from calibration
     calib_result_pickle = pickle.load(open("camera_calib_pickle.p", "rb"))
     mtx = calib_result_pickle["mtx"]
     optimal_camera_matrix = calib_result_pickle["optimal_camera_matrix"]
     dist = calib_result_pickle["dist"]
 
-    # create the cropped image based on camera image r'C:\Users\lowel\OneDrive\Pictures\cameraCal\img0.jpg'
     src = hsv_img
-    # distorted_image = cv2.rotate(src, cv2.ROTATE_180)
     undistorted_image = cv2.undistort(src, mtx, dist, None, optimal_camera_matrix)
 
     cropped_board = undistorted_image[202:412, 190:500]
@@ -62,9 +55,6 @@ def simple_ai():
 
     y1 = (height // 2)
     x1 = (width // 2)
-
-    # define an array that is the correct size with values that are all zeros
-    arr = np.zeros([6, 7], dtype=int)
 
     # Take the average hue of a certain range of pixels
     def average_color(i, j):
@@ -101,128 +91,149 @@ def simple_ai():
 
             else:
                 arr[i][j] = 0
-            print(i, j, h)
+            # print(i, j, h)
+    return arr
 
-    print(arr)
 
-    def diag1_check(i, j, color):
-        evaluation = 1
-        # checks values to the right and down of the point
-        m = 1
-        for x in range(3):
-            if ((i + m) > 5) or ((j + m) > 6):
-                break
-            elif arr[i + m][j + m] == color:
-                evaluation += 1
-                m += 1
-            else:
-                break
-        # checks values to the left and up of the point
-        n = 1
-        for x in range(3):
-            if ((i - n) < 0) or ((j - n) < 0):
-                break
-            elif arr[i - n][j - n] == color:
-                evaluation += 1
-                n += 1
-            else:
-                break
-        return evaluation
+def diag1_check(i, j, color, layout):
+    evaluation = 1
+    # checks values to the right and down of the point
+    m = 1
+    for x in range(3):
+        if ((i + m) > 5) or ((j + m) > 6):
+            break
+        elif layout[i + m][j + m] == color:
+            evaluation += 1
+            m += 1
+        else:
+            break
+    # checks values to the left and up of the point
+    n = 1
+    for x in range(3):
+        if ((i - n) < 0) or ((j - n) < 0):
+            break
+        elif layout[i - n][j - n] == color:
+            evaluation += 1
+            n += 1
+        else:
+            break
+    return evaluation
 
-    def diag2_check(i, j, color):
-        evaluation = 1
-        # checks values to the right and up of the point
-        m = 1
-        for x in range(3):
-            if ((i - m) < 0) or ((j + m) > 6):
-                break
-            elif arr[i - m][j + m] == color:
-                evaluation += 1
-                m += 1
-            else:
-                break
-        # checks values to the left and up of the point
-        n = 1
-        for x in range(3):
-            if ((i + n) > 5) or ((j - n) < 0):
-                break
-            elif arr[i + n][j - n] == color:
-                evaluation += 1
-                n += 1
-            else:
-                break
-        return evaluation
 
-    def verticle_check(i, j, color):
-        evaluation = 1
-        # checks values below the point
-        m = 1
-        for x in range(3):
-            if (i + m) > 5:
-                break
-            elif arr[i + m][j] == color:
-                evaluation += 1
-                m += 1
-            else:
-                break
-        return evaluation
+def diag2_check(i, j, color, layout):
+    evaluation = 1
+    # checks values to the right and up of the point
+    m = 1
+    for x in range(3):
+        if ((i - m) < 0) or ((j + m) > 6):
+            break
+        elif layout[i - m][j + m] == color:
+            evaluation += 1
+            m += 1
+        else:
+            break
+    # checks values to the left and up of the point
+    n = 1
+    for x in range(3):
+        if ((i + n) > 5) or ((j - n) < 0):
+            break
+        elif layout[i + n][j - n] == color:
+            evaluation += 1
+            n += 1
+        else:
+            break
+    return evaluation
 
-    def horizontal_check(i, j, color):
-        evaluation = 1
-        # checks values to the right of the point
-        m = 1
-        for x in range(3):
-            if (j + m) > 6:
-                break
-            elif arr[i][j + m] == color:
-                evaluation += 1
-                m += 1
-            else:
-                break
-        # checks values to the left of the point
-        n = 1
-        for x in range(3):
-            if (j - n) < 0:
-                break
-            elif arr[i][j - n] == color:
-                evaluation += 1
-                n += 1
-            else:
-                break
-        return evaluation
 
-    def evaluate(color):
-        evaluations = np.zeros((7), dtype=int)
-        for x in range(7):
-            for y in reversed(range(6)):
-                if arr[y][x] == 0:
-                    diagonal1 = diag1_check(y, x, color)
-                    diagonal2 = diag2_check(y, x, color)
-                    horizontal = horizontal_check(y, x, color)
-                    verticle = verticle_check(y, x, color)
-                    final_eval = max(diagonal1, diagonal2, horizontal, verticle)
-                    evaluations[x] = final_eval
-                    break
-        return evaluations
+def verticle_check(i, j, color, layout):
+    evaluation = 1
+    # checks values below the point
+    m = 1
+    for x in range(3):
+        if (i + m) > 5:
+            break
+        elif layout[i + m][j] == color:
+            evaluation += 1
+            m += 1
+        else:
+            break
+    return evaluation
 
-    red_evaluation = evaluate(-1)
-    yel_evaluation = evaluate(1)
 
-    print(yel_evaluation)
+def horizontal_check(i, j, color, layout):
+    evaluation = 1
+    # checks values to the right of the point
+    m = 1
+    for x in range(3):
+        if (j + m) > 6:
+            break
+        elif layout[i][j + m] == color:
+            evaluation += 1
+            m += 1
+        else:
+            break
+    # checks values to the left of the point
+    n = 1
+    for x in range(3):
+        if (j - n) < 0:
+            break
+        elif layout[i][j - n] == color:
+            evaluation += 1
+            n += 1
+        else:
+            break
+    return evaluation
+
+
+def evaluate(color, layout):
+    evaluations = np.zeros((7), dtype=int)
+    evaluations_y_pos = np.zeros((7), dtype=int)
+    for x in range(7):
+        for y in reversed(range(6)):
+            if layout[y][x] == 0:
+                evaluations_y_pos[x] = y  # create an array that has the y values of all of the lowest zeros
+                diagonal1 = diag1_check(y, x, color, layout)
+                diagonal2 = diag2_check(y, x, color, layout)
+                horizontal = horizontal_check(y, x, color, layout)
+                verticle = verticle_check(y, x, color, layout)
+                final_eval = max(diagonal1, diagonal2, horizontal, verticle)
+                evaluations[x] = final_eval
+                break
+    return evaluations, evaluations_y_pos
+
+
+def simple_ai(color, layout):
+    red_evaluation = evaluate(-1, layout)[0]
+    yel_evaluation = evaluate(1, layout)[0]
+    red_evaluation_y = evaluate(-1, layout)[1]
+    yel_evaluation_y = evaluate(1, layout)[1]
+    hypo_board = board.copy()
 
     if max(red_evaluation) >= 4:  # checks to see if
-        result = np.where(red_evaluation == np.amax(red_evaluation))
+        result = np.where(red_evaluation == np.amax(red_evaluation))[0]
         column = int(result[0])
     else:
-        result = np.where(yel_evaluation == np.amax(yel_evaluation))
-        if len(result) % 2 == 0:
-            n = random.randint(int(len(result)/2 - 1), int(len(result)/2))
-            column = int(result[n])
+        result = np.where(yel_evaluation == np.amax(yel_evaluation))[0]
+        copy_result = np.copy(result)
+        print(result)
+        for i in range(len(result)):
+            hypo_board[red_evaluation_y[result[i]]][result[i]] = 1  # updates the array with the best hypothetical solution
+            print('hypoboard', hypo_board)
+            hypo_eval = evaluate(-1, hypo_board)[0]  # evaluates board assuming its opponents turn
+            print('hypoeval' ,hypo_eval)
+            if max(hypo_eval) >= 4:  # checks to see if opponent will win
+                yel_evaluation[result[i]] = 0
+                copy_result = np.where(yel_evaluation == np.amax(yel_evaluation))[0]
+        if len(copy_result) % 2 == 0:
+            n = random.randint(int(len(result) / 2 - 1), int(len(result) / 2))
+            column = int(copy_result[n])
         else:
-            column = int(np.median(result))
-    print(result)
-    print(column)
+            column = int(np.median(copy_result))
+    print('column',column)
+    return column
 
+
+def move(column):
     x = 0.19
     y = 0.0381
     z = 0.3
@@ -250,7 +261,21 @@ def simple_ai():
     # .waitKey(0)
     # cv2.destroyAllWindows()
 
+#board = np.array([[ 0,  0,  0,  0 , 0 , 0 , 0],
+ #                [ 0 , 0 , 0 , 0 , 0 , 0 , 0],
+  #               [ 0, -1 , 0 , 0 , 0 , 0 , 0],
+   #              [ 0 , 1 , 0 , 0 , 0 , 0 ,-1],
+    #             [ 0 , 1 , 0 ,-1 ,-1 , 0 , 1],
+     #            [-1 , 1 , 1 , 1 ,-1 , 1 , 1]])
+
 
 for i in range(20):
+    robot = RobotArm()
+    cam_jpos = [0.00, -1.59174028, 1.03044239, 2.0943951, -0.09215338]
+    # define an array that is the size of the board with values that are all zeros
+    arr = np.zeros([6, 7], dtype=int)
     keyboard.wait('esc')
-    simple_ai()
+    board = take_pic()
+    take_pic()
+    pick = simple_ai(1, board)
+    move(pick)
--- Camera Calibration/test_image.py
+++ Camera Calibration/test_image.py
@@ -11,13 +11,15 @@ dist = calib_result_pickle["dist"]
 
 # create the cropped image based on camera image r'C:\Users\lowel\OneDrive\Pictures\cameraCal\img0.jpg'
 src = cv2.imread(r"C:\Users\lowel\PycharmProjects\helping-hands-ysp-david\frame.jpg")
-# distorted_image = cv2.rotate(src, cv2.ROTATE_180)
-undistorted_image = cv2.undistort(src, mtx, dist, None, optimal_camera_matrix)
+distorted_image = cv2.rotate(src, cv2.ROTATE_180)
+hsv_img = cv2.cvtColor(distorted_image, cv2.COLOR_RGB2HSV)
+undistorted_image = cv2.undistort(hsv_img, mtx, dist, None, optimal_camera_matrix)
 
 cropped_board = undistorted_image[188:397, 190:489]
 
-cv2.imshow('frame', cropped_board)
-cv2.imwrite('image.jpg', cropped_board)
+cv2.imshow('frame', distorted_image)
+cv2.imshow("frame2", cropped_board)
+#cv2.imwrite('image.jpg', cropped_board)
 cap.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
